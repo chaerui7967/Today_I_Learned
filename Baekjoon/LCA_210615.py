e3a4f81e39c47065d6c7b0eb743ccab5945dc994
rui <chaerui7967@gmail.com>
2021-06-26 17:59:04 +0900

    lca 복습

--- Baekjoon/LCA_210615.py
+++ Baekjoon/LCA_210615.py
@@ -7,25 +7,25 @@
 # 시간초과..
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(int(1e5)) # 런타임 오류 피하기
+sys.setrecursionlimit(int(1e5))  # 런타임 오류 피하기
 n = int(input())
 
 parent = [0] * (n+1)  # 부모 노드 정보
 d = [0] * (n+1)  # 각 노드까지의 깊이
 c = [0] * (n+1)  # 각 노드의 깊이가 계산되었는지 여부
-graph = [[] for _ in range(n+1)] # 그래프 정보
+graph = [[] for _ in range(n+1)]  # 그래프 정보 2중 리스트 [[---], [---]]
 
-for _ in range(n-1):
+for _ in range(n-1):  # 그래프 정보 해당 번호와 연결된 번호
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
 # 루트 노드부터 시작하여 깊이를 구하는 함수
-def dfs(x, depth):
+def dfs(x, depth):  # 깊이 우선 탐색을 통해서 부모 트리 입력
     c[x] = True
     d[x] = depth
     for y in graph[x]:
-        if c[y]: # 이미 깊이를 구했다면 패스
+        if c[y]:  # 이미 깊이를 구했다면 패스
             continue
         parent[y] = x
         dfs(y, depth + 1)
@@ -44,7 +44,7 @@ def lca(a, b):
         b = parent[b]
     return a
 
-dfs(1, 0)  # 루트 노드는 1번 노드
+dfs(1, 0)  # 루트노트 설정, 루트 노드는 1번 노드
 
 m = int(input())
 
@@ -52,11 +52,14 @@ for i in range(m):
     a, b = map(int, input().split())
     print(lca(a,b))
 
-# 다른 방법
+
+# 다른 방법 ----------------------------------------------------------------------------------------------
+## 부모 저장을 2**2만큼 저장
 
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
+
 def dfs(cur, d):
     depth[cur] = d
     visited[cur] = True
@@ -117,3 +120,28 @@ for _ in range(M):
     A, B = map(int, input().split())
     common_parent = lca(A, B)
     print(common_parent)
+
+
+# 입력             출력
+# 15              2
+# 1 2             4
+# 1 3             2
+# 2 4             1
+# 3 7             3
+# 6 2             1
+# 3 8
+# 4 9
+# 2 5
+# 5 11
+# 7 13
+# 10 4
+# 11 15
+# 12 5
+# 14 7   // 노드 정보
+# 6   // 출력할 결과 갯수
+# 6 11
+# 10 9
+# 2 6
+# 7 6
+# 8 13
+# 8 15

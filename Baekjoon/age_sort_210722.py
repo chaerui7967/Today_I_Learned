e3c62032faa3a5557b55581e0806e7a5e6d3ec21
chaerui7967 <85321888+chaerui7967@users.noreply.github.com>
2021-07-22 09:01:28 +0900

    Baekjoon 36day __ lambda 식 활용 주의.. 문제 자체는 쉬움

---
+++ Baekjoon/age_sort_210722.py
@@ -0,0 +1,19 @@
+# Silver 5_10814
+
+# 온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다.
+# 이때, 회원들을 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
+
+n = int(input())
+info = []
+
+for _ in range(n):
+    info.append((input().split()))
+
+info.sort(key= lambda info: (int(info[0])))
+
+for i in info:
+    print(i[0], i[1])
+
+
+
+

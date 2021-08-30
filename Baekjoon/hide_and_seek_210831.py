5bd823b9d92e40ebefe6f4ea00834a73f51ad380
chaerui7967 <85321888+chaerui7967@users.noreply.github.com>
2021-08-31 08:46:16 +0900

    Baekjoon 62day__숨바꼭질__ bfs사용

---
+++ Baekjoon/hide_and_seek_210831.py
@@ -0,0 +1,30 @@
+# Silver 1_1697
+
+# https://wook-2124.tistory.com/273 풀이
+
+# 수빈이는 동생과 숨바꼭질을 하고 있다.
+# 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고,
+# 동생은 점 K(0 ≤ K ≤ 100,000)에 있다. 수빈이는 걷거나 순간이동을 할 수 있다.
+# 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
+# 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+#
+# 수빈이와 동생의 위치가 주어졌을 때,
+# 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
+
+from collections import deque
+def bfs():
+    q = deque()
+    q.append(N)
+    while q:
+        v = q.popleft()
+        if v == K:
+            print(time[v])
+            return
+        for next_step in (v-1, v+1, v*2):
+            if 0 <= next_step < MAX and not time[next_step]:
+                time[next_step] = time[v] + 1
+                q.append(next_step)
+MAX = 100001
+N, K = map(int, input().split())
+time = [0]*MAX
+bfs()

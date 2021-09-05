d27ebf0b18e511b97390d7cc1db58faf0c07f906
chaerui7967 <85321888+chaerui7967@users.noreply.github.com>
2021-09-05 11:01:52 +0900

    Baekjoon 64day __ heapq와 deque를 적절히 사용하면 풀이 가능

--- Baekjoon/que_double_210905.py
+++ Baekjoon/que_double_210905.py
@@ -15,31 +15,36 @@
 
 # 우선순위 큐_heapq와 deque 사용으로 문제 풀이
 
-import sys;read=sys.stdin.readline
+import sys
 import heapq
 
-result=[]
+read = sys.stdin.readline
+result = []
 for T in range(int(read())):
-    visited=[False]*1_000_001
-    minH,maxH=[],[]
+    visited = [False]*1_000_001
+    minH, maxH = [], []
     for i in range(int(read())):
-        s=read().split()
-        if s[0]=='I':
-            heapq.heappush(minH,(int(s[1]),i))
-            heapq.heappush(maxH,(-int(s[1]),i))
-            visited[i]=True
-        elif s[1]=='1':
-            while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+        s = read().split()
+        if s[0] == 'I':
+            heapq.heappush(minH, (int(s[1]), i))
+            heapq.heappush(maxH, (-int(s[1]), i))
+            visited[i] = True
+        elif s[1] == '1':
+            while maxH and not visited[maxH[0][1]]:
+                heapq.heappop(maxH)
             if maxH:
                 visited[maxH[0][1]]=False
                 heapq.heappop(maxH)
         else:
-            while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+            while minH and not visited[minH[0][1]]:
+                heapq.heappop(minH)
             if minH:
                 visited[minH[0][1]]=False
                 heapq.heappop(minH)
-    while minH and not visited[minH[0][1]]:heapq.heappop(minH)
-    while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+    while minH and not visited[minH[0][1]]:
+        heapq.heappop(minH)
+    while maxH and not visited[maxH[0][1]]:
+        heapq.heappop(maxH)
     result.append(f'{-maxH[0][0]} {minH[0][0]}'if maxH and minH else'EMPTY')
 print('\n'.join(result))
 
